d9960e0e9b6931a68e2252659763eb5a61381933
蔡廷漳 <1019386658@qq.com>
2022-12-29 23:34:09 +0800

    22.12.29 add Country analyse

---
+++ country_info.py
@@ -0,0 +1,28 @@
+from main import *
+
+para_data = currency_data[currency_data["Date"] == "3/17/2022"][["Currency Code", "Currency Name", "Exchange Rate"]]
+merged_data = pd.merge(survey_data, para_data, left_on="Currency", right_on="Currency Code", how="outer", sort=False)
+merged_data.info()
+merged_data[(~merged_data["Currency"].isna()) & (merged_data["Currency Code"].isna())].index
+def conversion(index):
+    if (merged_data.loc[index, "CompFreq"] == "Weekly"):
+        merged_data.loc[index, "ConvertedToDollar"] = merged_data.loc[index, "CompTotal"] * 52 * merged_data.loc[index, "Exchange Rate"]
+    elif (merged_data.loc[index, "CompFreq"] == "Monthly"):
+        merged_data.loc[index, "ConvertedToDollar"] = merged_data.loc[index, "CompTotal"] * 12 * merged_data.loc[index, "Exchange Rate"]
+    elif (merged_data.loc[index, "CompFreq"] == "Yearly"):
+        merged_data.loc[index, "ConvertedToDollar"] = merged_data.loc[index, "CompTotal"] * 1 * merged_data.loc[index, "Exchange Rate"]
+    
+for i in merged_data.index:
+    conversion(i)
+plt.figure(figsize=(15, 15))
+plt.plot(survey_data[["Country", "CompTotal"]].groupby("Country").agg(np.mean).sort_values(by="CompTotal", ascending=False)[:50])
+plt.xticks(rotation=90)
+plt.show()
+real_data = merged_data[merged_data["ConvertedToDollar"] < 1000000.000]
+real_data.drop(real_data[real_data["DevType"].str.count(";") > 6].index, axis=0, inplace=True)
+real_data.drop(real_data[real_data["DevType"].isna()].index, inplace=True)
+country_df = real_data[["Country", "ConvertedToDollar"]].groupby("Country").agg(np.mean).sort_values("ConvertedToDollar", ascending=False)
+plt.figure(figsize=(10, 10))
+plt.title("Salaries by Country in Dollars($)")
+sns.barplot(y=country_df.index[:20], x=country_df["ConvertedToDollar"][:20], palette="Greens_r")
+plt.show()
---
+++ debug_info.py
@@ -0,0 +1,7 @@
+from main import *
+
+survey_data.head()
+schema_data.head()
+
+survey_data.info()
+schema_data.info()
